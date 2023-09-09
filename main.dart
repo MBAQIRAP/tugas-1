b38f2619dac23730761e1425176831ec57c22f5f
MBAQIRAP <92967062+MBAQIRAP@users.noreply.github.com>
2023-09-09 09:20:34 +0700

    usia

--- main.dart
+++ main.dart
@@ -6,10 +6,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya usia tahun");
+    int usia=DateTime.now().year-tahunLahir; 
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun");
   }
 }
 
 void main() {
   var mhs = Mahasiswa("H1D021068", "Muhammad Baqir Atmojo Pratomo", 2003);
+  mhs.perkenalan();
 }
